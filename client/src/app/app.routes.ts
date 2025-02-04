d38fcc565d57c263929eb87ca7f55df5b73b55a3
slava <kuncovs19@gmail.com>
2025-02-04 12:10:16 +0300

    feat: example components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component'
+import { ProfileComponent } from './shared/ui/components/profile/profile.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: HomePageComponent
+	},
+	{
+		path: 'profile',
+		component: ProfileComponent
+	},
+	{
+		path: '**',
+		component: NotFoundPageComponent
+	}
+]
