6a7e98df520b299e3fbe8e2500ccb925dda6a519
slava <kuncovs19@gmail.com>
2025-02-10 16:10:33 +0300

    feat: signalr notifications received on the client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ToastModule } from "primeng/toast";
+import { Subscription } from "rxjs";
+import { AuthService } from "../pages/auth/api/auth.service";
+import { NotificationService } from "./core/services/notification/notification.service";
+import { ToastService, ToastStatus } from "./core/services/toast";
 
 @Component({
   selector: "app-root",
@@ -10,6 +14,43 @@ import { ToastModule } from "primeng/toast";
 })
 export class AppComponent {
   authService = inject(AuthService);
+  toastService = inject(ToastService);
+  notificationService = inject(NotificationService);
+  private userSubscription!: Subscription;
+
+  notifications: string[] = [];
+  isUser = false;
+
   ngOnInit() {
     this.authService.authorize().subscribe((val) => console.log(val));
+
+    this.userSubscription = this.authService.isUser$.subscribe(
+      (isAuthenticated) => {
+        this.isUser = isAuthenticated;
+        if (isAuthenticated) {
+          console.log("User authenticated, starting notifications...");
+          this.notificationService.startConnection();
+        } else {
+          console.log("User logged out, stopping notifications...");
+          this.notificationService.stopConnection();
+        }
+      },
+    );
+
+    this.notificationService.notifications$.subscribe((messages) => {
+      if (messages.length > 0) {
+        const latestMessage = messages[messages.length - 1];
+        console.log("Получено сообщение:", latestMessage); 
+        this.toastService.showToast(
+          ToastStatus.Success,
+          latestMessage,
+        );
+      }
+    });
   }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+    this.notificationService.stopConnection();
+  }
+}
