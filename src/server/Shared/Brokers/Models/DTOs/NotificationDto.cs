b68b2226919f62103613e92ce1e4277d2bcb07fd
slava <kuncovs19@gmail.com>
2025-04-05 14:47:19 +0300

    fix: rollback changes in brokers project

---
+++ src/server/Shared/Brokers/Models/DTOs/NotificationDto.cs
@@ -0,0 +1,5 @@
+namespace Brokers.Models.DTOs;
+
+public record NotificationDto(
+	Guid UserId,
+	string Message);
--- src/server/Shared/Brokers/Services/Producers/RabbitMQProducer.cs
+++ src/server/Shared/Brokers/Services/Producers/RabbitMQProducer.cs
@@ -1,10 +1,7 @@
 ﻿using System.Text;
 using System.Text.Json;
-
 using Brokers.Interfaces;
-
 using Microsoft.Extensions.Logging;
-
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -29,7 +26,7 @@ public class RabbitMQProducer : RabbitMQBase, IRabbitMQProducer
 		await CreateQueueAsync<T>(cancellationToken);
 		await PublishBaseAsync<T>(body, cancellationToken);
 	}
-
+	
 	public async Task<TResponse> RequestReplyAsync<TRequest, TResponse>(TRequest message, Guid guid, CancellationToken cancellationToken)
 	{
 		var responseQueueName = $"{typeof(TResponse).Name}";
@@ -56,13 +53,13 @@ public class RabbitMQProducer : RabbitMQBase, IRabbitMQProducer
 			if (args.BasicProperties.CorrelationId == correlationId)
 			{
 				var body = JsonSerializer.Deserialize<TResponse>(
-							Encoding.UTF8.GetString(args.Body.ToArray()));
+					Encoding.UTF8.GetString(args.Body.ToArray()));
 
 				_logger.LogInformation("Get Recieved: " + Encoding.UTF8.GetString(args.Body.ToArray()));
 
 				tcs.TrySetResult((body!));
 
-				await _channel.QueueDeleteAsync(requestQueueName, cancellationToken: cancellationToken);
+				await _channel.QueueDeleteAsync(requestQueueName);
 			}
 			else
 			{
@@ -71,7 +68,7 @@ public class RabbitMQProducer : RabbitMQBase, IRabbitMQProducer
 				tcs.SetException(new InvalidOperationException("CorrelationId mismatch"));
 			}
 
-			await _channel.BasicAckAsync(args.DeliveryTag, multiple: false, cancellationToken: cancellationToken);
+			await _channel.BasicAckAsync(args.DeliveryTag, multiple: false);
 		};
 
 		await ConsumeBaseAsync(consumer, requestQueueName, cancellationToken);
@@ -82,8 +79,79 @@ public class RabbitMQProducer : RabbitMQBase, IRabbitMQProducer
 
 		_logger.LogInformation("Send request: " + properties.CorrelationId);
 
-		await using CancellationTokenRegistration ctr = cancellationToken.Register(tcs.SetCanceled);
+		using CancellationTokenRegistration ctr = cancellationToken.Register(tcs.SetCanceled);
 
 		return await tcs.Task;
 	}
+
+	// public async Task<TResponse> RequestReplyAsync<TRequest, TResponse>(
+	// 	TRequest message,
+	// 	Guid guid,
+	// 	CancellationToken cancellationToken)
+	// {
+	// 	var responseQueueName = $"{typeof(TResponse).Name}";
+	// 	var requestQueueName = $"{typeof(TRequest).Name}";
+	// 	
+	// 	try
+	// 	{
+	// 		var tcs = new TaskCompletionSource<TResponse>(
+	// 			TaskCreationOptions.RunContinuationsAsynchronously);
+	//
+	// 		var correlationId = guid.ToString();
+	//
+	// 		var properties = new BasicProperties
+	// 		{
+	// 			CorrelationId = correlationId,
+	// 			ReplyTo = requestQueueName
+	// 		};
+	//
+	// 		await CreateQueueAsync(requestQueueName, cancellationToken, true);
+	//
+	// 		var consumer = new AsyncEventingBasicConsumer(_channel);
+	//
+	// 		consumer.ReceivedAsync += async (model, args) =>
+	// 		{
+	// 			_logger.LogInformation(
+	// 				"CorrelationId: " + args.BasicProperties.CorrelationId + " - " + correlationId);
+	//
+	// 			if (args.BasicProperties.CorrelationId == correlationId)
+	// 			{
+	// 				var body = JsonSerializer.Deserialize<TResponse>(
+	// 					Encoding.UTF8.GetString(args.Body.ToArray()));
+	//
+	// 				_logger.LogInformation("Get Recieved: " + Encoding.UTF8.GetString(args.Body.ToArray()));
+	//
+	// 				tcs.TrySetResult(body!);
+	//
+	// 				await _channel.QueueDeleteAsync(requestQueueName, cancellationToken: cancellationToken);
+	// 			}
+	// 			else
+	// 			{
+	// 				_logger.LogWarning(
+	// 					"CorrelationId mismatch: " + args.BasicProperties.CorrelationId + " != " +
+	// 					correlationId);
+	//
+	// 				tcs.SetException(new InvalidOperationException("CorrelationId mismatch"));
+	// 			}
+	//
+	// 			await _channel.BasicAckAsync(args.DeliveryTag, false, cancellationToken);
+	// 		};
+	//
+	// 		await ConsumeBaseAsync(consumer, requestQueueName, cancellationToken);
+	//
+	// 		var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+	//
+	// 		await PublishBaseAsync(body, responseQueueName, properties, cancellationToken);
+	//
+	// 		_logger.LogInformation("Send request: " + properties.CorrelationId);
+	//
+	// 		await using var ctr = cancellationToken.Register(tcs.SetCanceled);
+	//
+	// 		return await tcs.Task;
+	// 	}
+	// 	finally
+	// 	{
+	// 		await _channel.QueueDeleteAsync(responseQueueName, cancellationToken: cancellationToken);
+	// 	}
+	// }
 }
