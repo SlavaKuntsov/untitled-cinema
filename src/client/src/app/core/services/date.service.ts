ef5a80b182eb0c945474fbbf7d144fed8a024c2f
slava <kuncovs19@gmail.com>
2025-04-06 03:49:42 +0300

    fix: copiot review

--- src/client/src/app/core/services/date.service.ts
+++ src/client/src/app/core/services/date.service.ts
@@ -7,7 +7,7 @@ export class DateService {
 		private _selectedDate = signal<Date | null>(null);
 		public selectedDate = this._selectedDate.asReadonly();
 	
-		updateSeatsArray(date: Date): void {
+		updateSelectedDate(date: Date): void {
 			this._selectedDate.set(date);
 		}
 }
